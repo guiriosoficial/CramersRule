453b03f911de252c6cb32c7ad6650168db41cd45
riwersxd <guilherme.rc.98@gmail.com>
2019-11-25 22:03:58 -0300

    Cria executavel

--- setup.py
+++
@@ -1,9 +0,0 @@
-from cx_Freeze import setup, Executable
-
-setup(
-    name='CRC',
-    options={'build_exe': {}},
-    version='1.0',
-    description='Cramers Rule Calculator with Laplace Theorem to Square Matrices Systems',
-    executables=[Executable('main.py', base=None)]
-)
